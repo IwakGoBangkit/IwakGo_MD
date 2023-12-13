22cabd62c31ffd3919575dec52a407605fe35705
Ahmad <ahmadkhusnudin58@gmail.com>
2023-12-13 14:01:50 +0700

    Make Result Scan UI
    Signed-off-by: Ahmad <ahmadkhusnudin58@gmail.com>

--- app/src/main/java/com/bangkit/fishery_app/ui/screen/scan_fish/ScanFishScreen.kt
+++ app/src/main/java/com/bangkit/fishery_app/ui/screen/scan_fish/ScanFishScreen.kt
@@ -9,31 +9,45 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.bangkit.fishery_app.R
 import com.bangkit.fishery_app.ui.screen.home.model.ImageResult
+import com.bangkit.fishery_app.ui.screen.scan_fish.common.runImageDetector
+import com.bangkit.fishery_app.ui.screen.scan_result.model.DetectionResult
+import kotlinx.coroutines.launch
 
 @Composable
 fun ScanFishScreen(
+    modifier: Modifier = Modifier,
     image: ImageResult?,
-    modifier: Modifier = Modifier
+    onDetectedImage: (List<DetectionResult>) -> Unit,
+    navigateToResult: () -> Unit,
 ) {
 
     ScanFishContent(
         image = image,
+        navigateToResult = navigateToResult,
+        onDetectedImage = onDetectedImage,
         modifier = modifier
     )
 }
 
 @Composable
 fun ScanFishContent(
+    modifier: Modifier = Modifier,
     image: ImageResult?,
-    modifier: Modifier = Modifier
+    onDetectedImage: (List<DetectionResult>) -> Unit,
+    navigateToResult: () -> Unit
 ) {
+    val context = LocalContext.current
+    val scope = rememberCoroutineScope()
+
     Column(
         verticalArrangement = Arrangement.spacedBy(16.dp),
         modifier = modifier
@@ -41,14 +55,22 @@ fun ScanFishContent(
             .padding(top = 128.dp, start = 32.dp, end = 32.dp, bottom = 32.dp)
     ) {
         AsyncImage(
-            model =image?.imageFile,
+            model = image?.imageFile,
             contentDescription = null,
             contentScale = ContentScale.Crop,
             modifier = modifier.size(328.dp)
         )
 
         Button(
-            onClick = { /*TODO*/ },
+            onClick = {
+//                scope.launch {
+//                    context.runImageDetector(
+//                        imageFile = image?.imageFile,
+//                        onDetectedImage = onDetectedImage
+//                    )
+//                }
+                navigateToResult()
+            },
             modifier = modifier.fillMaxWidth()
         ) {
             Text(text = stringResource(R.string.upload))
--- app/src/main/java/com/bangkit/fishery_app/ui/screen/scan_result/ScanResultScreen.kt
+++ app/src/main/java/com/bangkit/fishery_app/ui/screen/scan_result/ScanResultScreen.kt
@@ -1,13 +1,69 @@
 package com.bangkit.fishery_app.ui.screen.scan_result
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.hilt.navigation.compose.hiltViewModel
+import coil.compose.AsyncImage
+import com.bangkit.fishery_app.R
+import com.bangkit.fishery_app.ui.components.SectionText
+import com.bangkit.fishery_app.ui.screen.home.model.ImageResult
+import com.bangkit.fishery_app.ui.screen.scan_result.model.DetectionResult
 
 @Composable
-fun ScanResultScreen() {
-    
+fun ScanResultScreen(
+    image: ImageResult?,
+    viewModel: ScanResultViewModel = hiltViewModel()
+) {
+
+    ScanResultContent(
+//        detectionResult = ,
+        navigateToHome = {},
+        image = image,
+    )
 }
 
 @Composable
-fun ScanResultContent() {
-    
+fun ScanResultContent(
+    modifier: Modifier = Modifier,
+    image: ImageResult?,
+//    detectionResult: DetectionResult,
+    navigateToHome: () -> Unit
+) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+    ) {
+
+        AsyncImage(
+            model = image,
+            contentDescription = image?.imageFile.toString()
+        )
+
+        SectionText(title = stringResource(R.string.fish_type))
+
+        Text(
+            text = "",
+        )
+
+        SectionText(title = stringResource(R.string.fish_condition))
+        
+        Text(text = "")
+        
+        SectionText(title = stringResource(R.string.fish_treatment))
+        
+        Text(text = "")
+
+        OutlinedButton(
+            onClick = navigateToHome
+        ) {
+            Text(text = stringResource(R.string.back_to_home))
+        }
+    }
 }
